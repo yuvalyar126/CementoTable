da4197c8c43ee8a33ab9e97b54fdcd891240e224
yuvalyar12 <yuvalyar24@gmail.com>
2026-06-30 14:45:00 +0300

    add TableCell component

---
+++ src/components/GenericTable/TableCell.tsx
@@ -0,0 +1,33 @@
+import { memo } from 'react';
+import type { Column } from '../../types/types';
+import CellEditor from './CellEditor';
+
+interface TableCellProps {
+  column: Column;
+  value: any;
+  isEditing: boolean;
+  onStartEdit: () => void;
+  onSave: (newValue: any) => void;
+  onCancel: () => void;
+}
+
+function formatValue(value: any, column: Column) {
+  if (column.type === 'boolean') 
+  {
+    return value ? 'Yes' : 'No';
+  }
+
+  return value ?? '';
+}
+
+function TableCell({column, value, isEditing, onStartEdit, onSave, onCancel}: TableCellProps) {
+  return (
+    <td style={{width: column.width}}>
+      {isEditing ? 
+      (<CellEditor column={column} value={value} onSave={onSave} onCancel={onCancel}/>) :
+       (<div onClick={onStartEdit}> {formatValue(value, column)} </div>)}
+    </td>
+  );
+}
+
+export default memo(TableCell);
